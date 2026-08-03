44672972def1a6076ad1336a595272563cac8b87
Mohammadd Fahad Siddiqui <mohdfahadsiddiqui1248@gmail.com>
2026-08-03 16:25:49 +0530

    Revert "Merge branch 'main' of https://github.com/mohammadfahadsiddiqui/Endless_Runner"
    This reverts commit 7b015266c073b976aecb7c0d54a6d763ef521deb, reversing
    changes made to 42465e9e498fa619154c269c70ac4e06fa52f5df.

--- Assets/Scripts/SegmentGenerator.cs
+++ Assets/Scripts/SegmentGenerator.cs
@@ -1,33 +1,28 @@
 using System.Collections;
 using UnityEngine;
 
-public class SegmentGenerator : MonoBehaviour
+public class Segment : MonoBehaviour
 {
     public GameObject[] segment;
 
-    [SerializeField] int zPos = 50;
+    [SerializeField] int zPos = 0;
     [SerializeField] bool creatingSegment = false;
     [SerializeField] int segmentNum;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Update()
+    void Start()
     {
-        if (creatingSegment == false)
-        {
-            creatingSegment = true;
-            StartCoroutine(SegmentGen());
-        }
+       StartCoroutine(SegmentGen()); 
     }
 
     IEnumerator SegmentGen()
     {
-        segmentNum = Random.Range(0, 3);
-        Instantiate(segment[segmentNum], new Vector3(0, 0, zPos), Quaternion.identity);
-        zPos += 50;
+        segmentNum = Random.Range(0, segmen);
+        yield return new WaitForSeconds(10);
+        segmentMap02.SetActive(true);
 
-        yield return new WaitForSeconds(3);
-        creatingSegment = false;
-
+        yield return new WaitForSeconds(10);
+        segmentMap03.SetActive(true);
     }
 
 }
